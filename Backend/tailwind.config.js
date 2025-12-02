ba316340787825bb42ce3c3ba2aec8959bf8713c
Ashok Sah <asah868@rku.ac.in>
2025-12-02 22:00:13 +0530

    update tailwind css '

---
+++ Backend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.ejs",
+    "./views/partials/**/*.ejs",
+    "./public/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
