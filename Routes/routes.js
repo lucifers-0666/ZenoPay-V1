2787a9694e91b58c6ad3968ce60730758b2540f0
lucifers-0666 <zaidamreliya002@gmail.com>
2026-03-14 18:48:30 +0530

    Remove privacy policy related files and JavaScript functionality

--- Routes/routes.js
+++ Routes/routes.js
@@ -30,7 +30,6 @@ const ReferralController = require("../Controllers/ReferralController");
 const EmailVerificationController = require("../Controllers/EmailVerificationController");
 const LegalPagesController = require("../Controllers/LegalPagesController");
 const ContactController = require("../Controllers/ContactController");
-const PrivacyPolicyController = require("../Controllers/PrivacyPolicyController");
 const PricingController = require("../Controllers/PricingController");
 const BeneficiaryController = require("../Controllers/BeneficiaryController");
 const SystemStatusController = require("../Controllers/SystemStatusController");
@@ -786,13 +785,6 @@ router.get("/ref/:code", ReferralController.handleReferralLink);
 
 // Legal Pages Routes
 router.get("/terms", LegalPagesController.getTermsPage);
-router.get("/privacy", PrivacyPolicyController.getPrivacyPolicy); // Dynamic Privacy Policy
-router.get("/privacy-policy", PrivacyPolicyController.getPrivacyPolicy); // Alias
-router.get("/privacy-policy/archive", PrivacyPolicyController.getPrivacyPolicyArchive);
-router.get("/privacy-policy/v/:version", PrivacyPolicyController.getPrivacyPolicyVersion);
-router.get("/privacy-policy/compare", PrivacyPolicyController.comparePrivacyPolicyVersions);
-router.get("/privacy-policy/download", PrivacyPolicyController.downloadPrivacyPolicyPDF);
-router.post("/api/privacy-policy/accept", PrivacyPolicyController.acceptPrivacyPolicy);
 router.get("/about", LegalPagesController.getAboutPage);
 router.get("/help", LegalPagesController.getHelpPage);
 router.get("/faq", LegalPagesController.getHelpPage); // Alias for /help
